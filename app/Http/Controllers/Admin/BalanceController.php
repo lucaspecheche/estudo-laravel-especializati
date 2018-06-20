d116f913994a4289ba6b0c05953e5aacc805b3b4
Lucas Lima <lucaspecheche@gmail.com>
2018-06-20 03:15:07 -0400

    #18 - Criado Formulario de recarga

--- app/Http/Controllers/Admin/BalanceController.php
+++ app/Http/Controllers/Admin/BalanceController.php
@@ -14,4 +14,14 @@ class BalanceController extends Controller
 
         return view('admin.balance.index', compact('amount'));
     }
+
+    public function deposit()
+    {
+    	return view('admin.balance.deposit');
+    }
+
+    public function depositStore(Request $request)
+    {
+    	
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,12 @@
 */
 
 Route::group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'admin'], function(){
+	Route::post ('deposit', 'BalanceController@depositStore')->name('deposit.store');
+	Route::get ('deposit', 'BalanceController@deposit')->name('balance.deposit');
     Route::get ('/', 'AdminController@index')->name('admin.home');
     Route::get ('balance', 'BalanceController@index')->name('admin.balance');
 });
+
 
 Route::get('/', 'Site\SiteController@index')->name('home');
 
